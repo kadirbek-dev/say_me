2b3e4f0b7aa6ca98da7d7da1368564f62b65b514
kadirbek-dev <kadirbekbeka2012@gmail.com>
2026-08-25 13:02:03 +0300

    Fix R8 minify in gradle build

--- say_me/android/app/build.gradle.kts
+++ say_me/android/app/build.gradle.kts
@@ -25,6 +25,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
